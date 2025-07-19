26acd6b2168de9989c99883ef987d850d3e575d2
MinKhantKyaw <minkhant050603@gmail.com>
2025-07-19 23:42:40 +0700

    Update metadata.ts

--- app/metadata.ts
+++ app/metadata.ts
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://sandtechth.vercel.app"),
-  title: "Sandtech Engineering Company Limited",
+  title: "Sandtech Engineering Co.,Ltd,.",
   description: "Quality construction services with integrity and professionalism.",
   openGraph: {
     title: "Sandtech Engineering Company Limited",
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
     siteName: "Sandtech Engineering",
     images: [
       {
-        url: "/assets/sandtechtemplate.png",
+        url: "https://sandtechth.vercel.app/assets/sandtechtemplate.png", // absolute URL
         width: 1200,
         height: 630,
         alt: "Sandtech Engineering Logo",
@@ -24,6 +24,8 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "Sandtech Engineering Company Limited",
     description: "Quality construction services with integrity and professionalism.",
-    images: ["/assets/sandtechtemplate.png"],
+    images: [
+      "https://sandtechth.vercel.app/assets/sandtechtemplate.png", // absolute URL
+    ],
   },
 };
